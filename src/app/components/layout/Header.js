d4f0da8ae12ebdbd6e83a8ed3551158f5abf7120
GiovannaSilverio <giosilverio.10@gmail.com>
2023-07-21 17:56:04 -0300

    antes de criar a branch header

---
+++ src/app/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='bg-marromescuro h-77px text-bege'>
+      <img src="" alt="Logo" />
+      <nav className="flex items-center">
+        <ul className='flex'>
+          <li>Home</li>
+          <li>Sobre Nós</li>
+          <li>Cardápio</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
